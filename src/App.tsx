9110a9103181d873188a7d801abb7ee02e4e945e
Tamam Krajem <tamamkarim71@gmail.com>
2026-01-28 15:17:33 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//import './App.css';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <>
+      <h1>My Media sharing app</h1>
+      <Home />
+    </>
+  );
+};
+
+export default App;
---
+++ src/types/DBTypes.ts
@@ -0,0 +1,12 @@
+export type MediaItem = {
+  media_id: number;
+  user_id: number;
+  filename: string;
+  thumbnail: string;
+  filesize: number;
+  media_type: string;
+  title: string;
+  description: string;
+  created_at: string;
+  screenshots: string[];
+};
